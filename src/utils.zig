d85e1a4b63a83cc7c78c5546d35beb4b2f13cfe0
Jafagervik <jorgenafagervik@gmail.com>
2025-01-26 23:31:13 +0100

    clean up utils

--- src/utils.zig
+++ src/utils.zig
@@ -16,15 +16,6 @@ pub fn getRandomNumber() !f32 {
 }
 
 pub fn getRandomNumberBetween(lo: f32, hi: f32) !f32 {
-    var seed: u64 = undefined;
-    std.posix.getrandom(std.mem.asBytes(&seed)) catch |err| {
-        std.debug.print("Failed to get random seed: {}\n", .{err});
-        return error.RandSeedError;
-    };
-
-    // Needed in 0.14.234523454 dev something
-    var prng = std.Random.DefaultPrng.init(seed);
-    const rnd = prng.random();
-
-    return lo + (rnd.float(f32) * (hi - lo));
+    const random_number = try getRandomNumber();
+    return lo + (random_number * (hi - lo));
 }
