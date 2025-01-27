507d4fa6641acf55e0b590817e1d7128a5cdd609
Jafagervik <jorgenafagervik@gmail.com>
2025-01-27 16:30:22 +0100

    fixed root

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
+
 pub const Tensor = @import("tensor.zig").Tensor;
 pub const SGD = @import("optim.zig").SGD;
-pub const loss = @import("loss.zig");
 
 test {
     std.testing.refAllDecls(@This());
