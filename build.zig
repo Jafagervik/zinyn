e511ad0bf5a6be5753bc76f090b70a833d0e2aa8
Jafagervik <jorgenafagervik@gmail.com>
2025-01-27 18:15:58 +0100

    move to zybel

--- build.zig
+++ build.zig
@@ -2,59 +2,40 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
+    const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("zinyn", .{
-        .root_source_file = b.path("src/root.zig"),
+    _ = b.addModule("zybel", .{
+        .root_source_file = b.path("src/zybel.zig"),
     });
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zinyn",
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-        .version = .{ .major = 0, .minor = 0, .patch = 2 },
-    });
-
-    b.installArtifact(lib);
-
-    const exe = b.addExecutable(.{
-        .name = "zinyna",
-        .root_source_file = b.path("src/main.zig"),
+    const tests = b.addTest(.{
+        .root_source_file = b.path("src/zybel.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const run_lib_unit_tests = b.addRunArtifact(tests);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
+
+// const exe = b.addExecutable(.{
+//     .name = "zinyna",
+//     .root_source_file = b.path("src/main.zig"),
+//     .target = target,
+//     .optimize = optimize,
+// });
+//
+// b.installArtifact(exe);
+//
+// const run_cmd = b.addRunArtifact(exe);
+//
+// run_cmd.step.dependOn(b.getInstallStep());
+//
+// if (b.args) |args| {
+//     run_cmd.addArgs(args);
+// }
+//
+// const run_step = b.step("run", "Run the app");
+// run_step.dependOn(&run_cmd.step);
