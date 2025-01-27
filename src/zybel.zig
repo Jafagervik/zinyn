ce5e16af067aa6f090145ee961ed7f06cb635d75
Jafagervik <jorgenafagervik@gmail.com>
2025-01-27 18:44:43 +0100

    optim

--- src/zybel.zig
+++ src/zybel.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub const Tensor = @import("tensor.zig").Tensor;
-pub const SGD = @import("optim.zig").SGD;
+pub const optim = @import("optim.zig");
 
 test {
     std.testing.refAllDecls(@This());
