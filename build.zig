4bb2c4c54c64ab80b96c478a52a1f2e29fc13387
Jafagervik <jorgenafagervik@gmail.com>
2025-01-27 19:21:27 +0100

    0.0.3

--- build.zig
+++ build.zig
@@ -20,23 +20,3 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
 }
-
-// const exe = b.addExecutable(.{
-//     .name = "zinyna",
-//     .root_source_file = b.path("src/main.zig"),
-//     .target = target,
-//     .optimize = optimize,
-// });
-//
-// b.installArtifact(exe);
-//
-// const run_cmd = b.addRunArtifact(exe);
-//
-// run_cmd.step.dependOn(b.getInstallStep());
-//
-// if (b.args) |args| {
-//     run_cmd.addArgs(args);
-// }
-//
-// const run_step = b.step("run", "Run the app");
-// run_step.dependOn(&run_cmd.step);
